a979ad5ca7a951813514540fd0ec842cabe8e50d
pere.berge@itequia.com <pere.berge@LTP00015>
2018-04-13 10:35:56 +0200

    file structure

---
+++ src/renderer/components/shell.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="wrapper">
+    <main>
+      <Type-box></Type-box>
+      <Result-box></Result-box>
+    </main>
+  </div>
+</template>
+
+<script>
+  import TypeBox from './type-box/index'
+  import ResultBox from './result-box/index'
+
+  export default {
+    name: 'shell',
+    components: { TypeBox, ResultBox },
+    data () {
+      return {
+        electron: process.versions['atom-shell'],
+        name: this.$route.name,
+        node: process.versions.node,
+        path: this.$route.path,
+        platform: require('os').platform(),
+        vue: require('vue/package.json').version,
+        username: require('os').userInfo().username
+      }
+    },
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    }
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body { font-family: 'Source Sans Pro', sans-serif; }
+
+  #wrapper {
+    background: black;
+    height: 100vh;
+    padding: 60px 80px;
+    width: 100vw;
+  }
+
+  main {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  main > div { flex-basis: 50%; }
+
+</style>
---
+++ src/renderer/components/type-box/index.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="left-side">
+      <span class="title">
+        {{ username }} {{ path}}
+      </span>
+      <div class="type-box">
+        <input type="text" name="" value="" class="shell-input" placeholder="|">
+      </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Type-box',
+    data () {
+      return {
+        electron: process.versions['atom-shell'],
+        name: this.$route.name,
+        node: process.versions.node,
+        path: this.$route.path,
+        platform: require('os').platform(),
+        vue: require('vue/package.json').version,
+        username: require('os').userInfo().username
+      }
+    },
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    }
+  }
+</script>
+
+<style>
+
+  .left-side {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .title {
+    color: #ffffff;
+    font-size: 17px;
+    margin-bottom: 6px;
+  }
+
+  .title.alt {
+    font-size: 18px;
+    margin-bottom: 10px;
+  }
+
+  .blink {
+    animation: blink-animation 1s steps(5, start) infinite;
+    -webkit-animation: blink-animation 1s steps(5, start) infinite;
+    color: white;
+  }
+  @keyframes blink-animation {
+    to {
+      visibility: hidden;
+    }
+  }
+  @-webkit-keyframes blink-animation {
+    to {
+      visibility: hidden;
+    }
+  }
+  .type-box {
+    display: flex;
+    flex-direction: row;
+  }
+  .shell-input {
+    color: white;
+    background: black;
+    border: none;
+    padding-left: 10px;
+  }
+  .shell-input:focus {
+    outline: 0
+  }
+  .path {
+    color: white;
+  }
+</style>
