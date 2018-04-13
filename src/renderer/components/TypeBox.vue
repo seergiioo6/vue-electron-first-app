d8b16cd891172698b4605c447ec8b46a40a608a0
Andrea Corda <andrea.corda@itequia.com>
2018-04-13 13:01:16 +0200

    comando cd

---
+++ src/renderer/components/TypeBox.vue
@@ -0,0 +1,121 @@
+<template>
+	<div id="input-box">
+      <div class="header white">
+        Microsoft Windows [Version 10.0.16299.371]
+		<br/>
+        (c) 2017 Microsoft Corporation. All rights reserved.
+        <br/><br/>
+        {{ username }} {{ path}}
+      </div>
+      <div class="input white">
+        <span class="blink">$ <input v-model="text" @keydown="input" type="text" name="" value="" class="shell-input"/></span>
+      </div>
+	</div>
+</template>
+
+<script>
+  import { mapActions } from 'vuex'
+  import open from '../../services/open'
+  import list from '../../services/list'
+  import { changePath } from '../../services/cd' 
+  export default {
+    name: 'Type-box',
+    data () {
+      return {
+        electron: process.versions['atom-shell'],
+        name: this.$route.name,
+        node: process.versions.node,
+        path: this.$route.path,
+        platform: require('os').platform(),
+        vue: require('vue/package.json').version,
+        username: require('os').userInfo().username,
+        text: '',
+        commands: [
+          'ls',
+          'mkdir',
+          'cd',
+          'open'
+        ]
+      }
+    },
+    methods: {
+      ...mapActions([
+        'setOutput'
+      ]),
+      input (e) {
+        if (e.key === 'Enter') {
+          let keyWord = this.text.replace(/ .*/, '')
+          if (this.commands.includes(keyWord)) {
+            if (this.text.includes('open')) {
+              this.$electron.shell.openExternal(open.getUrl(this.text))
+            } else if (this.text === 'ls') {
+              let files = list.getFiles(this.path)
+              this.setOutput({
+                type: 'ls',
+                content: files
+              })
+            } else if (this.text.startsWith('cd')){
+              changePath(this.text)
+            }
+          } else {
+            console.log('no se reconoce el comando')
+          }
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+	%courier{
+		font-size: 16px;
+		font-family: 'Courier New', Courier, monospace;
+	}
+
+	.left-side {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.header{
+		@extend %courier;
+	}
+	.title {
+		color: #ffffff;
+		font-size: 17px;
+		margin-bottom: 6px;
+	}
+
+	.title.alt {
+		font-size: 18px;
+		margin-bottom: 10px;
+	}
+
+	.type-box {
+		display: flex;
+		flex-direction: row;
+	}
+	.shell-input {
+		color: white;
+		background: black;
+		border: none;
+		padding-left: 10px;
+		&:focus{
+			outline: 0;
+		}
+		@extend %courier;
+	}
+
+
+	.white{
+		color: white;
+	}
+
+	/* Keyframes */
+	@keyframes blink {
+		50% {
+		opacity: 0;
+		}
+	}
+
+</style>
---
+++ src/renderer/services/cd.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+import store from '../store';
+
+import list from 'list';
+
+export default {
+    changePath(comand) {
+        let newFolder = this.getPath(comand);
+        if(list.getFiles().includes(newFolder)) {
+            const oldPath = store.getPath()
+            const newPath = oldPath + '/' +newFolder
+            store.dispatch('SET_PATH', newPath)
+        } else {
+
+        }
+    },
+
+    getPath(comand){
+        return comand.substring(comand.indexOf('cd ') + 5, text.length);
+    }
+}
